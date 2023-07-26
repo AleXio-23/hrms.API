3877f6ea400bc248faca66fdb1fa53fe39fc2846
AleXio-23 <alekogabelashvili777@gmail.com>
2023-07-26 14:23:31 +0400

    fix IGetUsersWorkSchedulesService

--- hrms.API/hrms.Application/Services/User/UsersWorkSchedule/GetUsersWorkSchedules/IGetUsersWorkSchedulesService.cs
+++ hrms.API/hrms.Application/Services/User/UsersWorkSchedule/GetUsersWorkSchedules/IGetUsersWorkSchedulesService.cs
@@ -1,9 +1,10 @@
 using hrms.Domain.Models.User;
+using hrms.Shared.Models;
 
 namespace hrms.Application.Services.User.UsersWorkSchedule.GetUsersWorkSchedules
 {
     public interface IGetUsersWorkSchedulesService
     {
-        Task<UsersWorkScheduleDTO> Execute(UsersWorkScheduleFilter filter, CancellationToken cancellationToken);
+        Task<ServiceResult<List<UsersWorkScheduleDTO>>> Execute(UsersWorkScheduleFilter filter, CancellationToken cancellationToken);
     }
 }
