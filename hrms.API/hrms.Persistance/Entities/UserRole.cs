e55adc76eaea501fc0c1dacdd67962383a995c4d
AleXio-23 <alekogabelashvili777@gmail.com>
2023-10-10 12:36:42 +0400

    update user roles

--- hrms.API/hrms.Persistance/Entities/UserRole.cs
+++ hrms.API/hrms.Persistance/Entities/UserRole.cs
@@ -7,7 +7,9 @@ public partial class UserRole
 {
     public int UserId { get; set; }
 
-    public int RoleId { get; set; }
+    public int? UserId { get; set; }
+
+    public int? RoleId { get; set; }
 
     public virtual Role Role { get; set; } = null!;
 
